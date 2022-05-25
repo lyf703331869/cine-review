915bf0dd8ffb667671b194b0b9de521f4139c5c2
Shannen Bendeck <shay@shannens-mbp.myfiosgateway.com>
2022-05-25 15:52:35 -0400

    got search console to work

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,19 @@
+const api = "14e1acd020ccd7e75b55a6e59233cf2c";
+const url =
+  "https://api.themoviedb.org/3/search/movie?api_key=14e1acd020ccd7e75b55a6e59233cf2c&query=harry";
 
-var url = "https://developers.themoviedb.org/3/movies/get-movie-details";
-var api = "795237d1f5c251b1695453597353c8fd";
-var newUrl =
-  "https://api.themoviedb.org/3/movie/top_rated?api_key=795237d1f5c251b1695453597353c8fd&language=en-US&page=1&with_genres=18";
-
-// Selecting the elements from the document aka DOM
 var searchBtn = document.querySelector(".button");
 var input = document.querySelector("#input");
 
 // $(".searchBtn").on("click", checkurl);
 
 searchBtn.addEventListener("click", function () {
-  console.log("this yo");
+  const value = input.value;
 
-  // get the input value
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Data: ", data);
+    });
+  console.log("Value: ", value);
 });
-
-// main part
-var imoveurl =
-  "https://api.themoviedb.org/3/movie/top_rated?api_key=795237d1f5c251b1695453597353c8fd&language=en-US&page=1&with_genres=18";
-
-fetch(imoveurl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    for (i = 0; i < data.results.length; i++) {
-      var movieName = data.results[i].title;
-      var posterId = data.results[i].poster_path;
-      var voteScore = data.results[i].vote_average;
-      var photoUrl =
-        "https://www.themoviedb.org/t/p/w440_and_h660_face" + posterId;
-      $(".movieCard").append(
-        "<div class='column'>" +
-          "<div class='callout'>" +
-          `<p><img src="${photoUrl}"/></p>` +
-          `<a class="modalLink" data-bs-toggle="modal" data-bs-target="#movieModal">${movieName}</a>` +
-          `<p>${voteScore}</p>` +
-          "</div>" +
-          "</div>"
-      );
-    }
-  });
-
