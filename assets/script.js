5d6a9277885ff512e0ec84ec078ab61d36c3aea9
lyf703331869 <yliu52@nyit.edu>
2022-05-25 12:06:27 -0400

    adding new APIs

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 var url = "https://developers.themoviedb.org/3/movies/get-movie-details";
 var api = "795237d1f5c251b1695453597353c8fd";
 var newUrl =
@@ -32,14 +31,18 @@ fetch(imoveurl)
       var photoUrl =
         "https://www.themoviedb.org/t/p/w440_and_h660_face" + posterId;
       $(".movieCard").append(
-        "<div class='column'>" +
-          "<div class='callout'>" +
-          `<p><img src="${photoUrl}"/></p>` +
-          `<a class="modalLink" data-bs-toggle="modal" data-bs-target="#movieModal">${movieName}</a>` +
-          `<p>${voteScore}</p>` +
-          "</div>" +
-          "</div>"
+        // "<div class='column'>" +
+        //   "<div class='callout'>" +
+        //   `<p><img src="${photoUrl}"/></p>` +
+        //   `<a class="modalLink" data-bs-toggle="modal" data-bs-target="#movieModal">${movieName}</a>` +
+        //   `<p>${voteScore}</p>` +
+        //   "</div>" +
+        //   "</div>"
+        `
+        <div>
+            <div>${photoUrl}</div>
+        </div>
+        `
       );
     }
   });
-
