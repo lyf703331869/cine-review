01bdfccdb58816dd23793e43b0535192045adea9
Shannen Bendeck <shay@shannens-mbp.myfiosgateway.com>
2022-05-25 21:30:19 -0400

    updates on java

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-var baseUrl = "https://api.themoviedb.org/3";
-=======
-var url = "https://developers.themoviedb.org/3/movies/get-movie-details";
->>>>>>> f4eda2f99b70bb20538b29664f2bf4fa75914d22
 var api = "795237d1f5c251b1695453597353c8fd";
 var searchPath =
   baseUrl +
@@ -16,11 +11,9 @@ var input = document.querySelector("#input");
 
 // $(".searchBtn").on("click", checkurl);
 
-searchBtn.addEventListener("click", function () {
-});
+searchBtn.addEventListener("click", function () {});
 
 // Break down for team on header:
-
 
 //slide
 let slideIndex = 0;
@@ -39,7 +32,6 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
 
 // main part
 var imoveurl =
@@ -66,76 +58,78 @@ fetch(imoveurl)
           "</div>"
       );
     }
-<<<<<<< HEAD
-
-=======
   });
-  //carousel
-fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=795237d1f5c251b1695453597353c8fd")
-.then(res => res.json())
-.then(data => {
-  // populate the posters
-  let movies = data.results;
-  let posterIndexes = generateRandomPhotos();
+//carousel
+fetch(
+  "https://api.themoviedb.org/3/trending/movie/week?api_key=795237d1f5c251b1695453597353c8fd"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    // populate the posters
+    let movies = data.results;
+    let posterIndexes = generateRandomPhotos();
 
-  let count = 0;
+    let count = 0;
 
-  // loop through the movies
-  for (let i = 0; i < movies.length; i++) {
-    // movie backdrops for the four selected movies
-    if (posterIndexes.includes(i)) {
-      // append the image to the carousel
-      document.getElementById("slides-container").innerHTML += `
+    // loop through the movies
+    for (let i = 0; i < movies.length; i++) {
+      // movie backdrops for the four selected movies
+      if (posterIndexes.includes(i)) {
+        // append the image to the carousel
+        document.getElementById("slides-container").innerHTML += `
     <li class="${count == 0 ? "is-active " : ""}orbit-slide">
       <figure class="orbit-figure">
-        <img class="orbit-image" src="${"https://image.tmdb.org/t/p/original/" + movies[i].backdrop_path}" alt="${movies[i].original_title}">
-        <figcaption class="orbit-caption">${movies[i].original_title}</figcaption>
+        <img class="orbit-image" src="${
+          "https://image.tmdb.org/t/p/original/" + movies[i].backdrop_path
+        }" alt="${movies[i].original_title}">
+        <figcaption class="orbit-caption">${
+          movies[i].original_title
+        }</figcaption>
       </figure>
     </li>  
     `;
 
+        count++;
+      }
+    }
+
+    $(document).foundation();
+  })
+  .catch(() => {
+    alert("error loading carousel");
+  });
+
+// function to generate random numbers for four posters between 0 and 19
+function generateRandomPhotos() {
+  let photoIndexes = [];
+
+  let count = 0;
+
+  while (count < 4) {
+    let randomNumber = getRandomInt(0, 19);
+
+    // loop through the photoIndexes array to find if it has already been chosen
+    let found = false;
+
+    for (let i = 0; i < photoIndexes.length; i++) {
+      if (photoIndexes[i] == randomNumber) {
+        found = true;
+      }
+    }
+
+    // if not found, add it to the indexes array and increment count
+    if (!found) {
+      photoIndexes.push(randomNumber);
       count++;
     }
   }
 
-  $(document).foundation();
-})
-.catch(() => {
-  alert('error loading carousel')
-});
-
-// function to generate random numbers for four posters between 0 and 19
-function generateRandomPhotos() {
-let photoIndexes = [];
-
-let count = 0;
-
-while (count < 4) {
-  let randomNumber = getRandomInt(0, 19);
-
-  // loop through the photoIndexes array to find if it has already been chosen
-  let found = false;
-
-  for (let i = 0; i < photoIndexes.length; i++) {
-    if (photoIndexes[i] == randomNumber) {
-      found = true;
-    }
-  }
-
-  // if not found, add it to the indexes array and increment count
-  if (!found) {
-    photoIndexes.push(randomNumber);
-    count++;
-  }
+  return photoIndexes;
 }
-
-return photoIndexes;
-}
->>>>>>> f4eda2f99b70bb20538b29664f2bf4fa75914d22
 
 // function to generate number between min and max (inclusive)
 function getRandomInt(min, max) {
-min = Math.ceil(min);
-max = Math.floor(max);
-return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
